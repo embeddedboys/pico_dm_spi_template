30a3bfc8aab1e915c1b23282742f2cdfb8d8a636
iotah <writeforever@foxmail.com>
2024-05-23 07:32:52 +0800

    update st7789 pin define
    Signed-off-by: iotah <writeforever@foxmail.com>

--- src/cmake/st7789.cmake
+++ src/cmake/st7789.cmake
@@ -5,12 +5,12 @@
 
 # LCD Pins for SPI and PIO interface
 set(TFT_SPIX      0)    # which spi controller will be used.
-set(TFT_SCL_PIN   18)
-set(TFT_SDA_PIN   19)
+set(TFT_SCL_PIN   10)
+set(TFT_SDA_PIN   11)
 set(TFT_RES_PIN   15)
 set(TFT_DC_PIN    14)
-set(TFT_CS_PIN    13)
-set(TFT_BLK_PIN   12)
+set(TFT_CS_PIN    9)
+set(TFT_BLK_PIN   13)
 set(TFT_HOR_RES   240)
 set(TFT_VER_RES   320)
 set(TFT_BUS_CLK_KHZ 62500)
